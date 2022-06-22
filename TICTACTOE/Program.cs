2c0e309aeb14af132505658d6d0c784a7a488d8f
HatimDalal <dalalhatim16@gmail.com>
2022-06-22 17:13:46 +0530

    boardready

--- TICTACTOE/Program.cs
+++ TICTACTOE/Program.cs
@@ -6,8 +6,26 @@ namespace TICTACTOE
     {
         static void Main(string[] args)
         {
-            
+            SetField();
+            Console.ReadKey();
         }
+        public static void SetField()
+        {
+            Console.WriteLine("     |     |     ");
+            //Todo replace with vars
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
 
+            //Todo replace with vars
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+
+            //Todo replace with vars
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("     |     |     ");
+
+        }
     }
 }
