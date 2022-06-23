2d24a4d43260d208232a53284f9dfed891a533c0
HatimDalal <dalalhatim16@gmail.com>
2022-06-23 12:42:58 +0530

    checking-fields.

--- TICTACTOE/Program.cs
+++ TICTACTOE/Program.cs
@@ -2,30 +2,115 @@
 
 namespace TICTACTOE
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+
+        //Wecreate the playfield here.
+        static char[,] playField =
         {
-            SetField();
-            Console.ReadKey();
+            {'1','2','3'},
+            {'4','5','6'},
+            {'7','8','9'}
+        };
+
+        private static void Main(string[] args)
+        {
+            int player = 2;
+            int input = 0;
+            bool inputCorrect = true;
+
+            do
+            {
+                if (player == 2)
+                {
+                    player = 1;
+                    EnterXorO(player,input);
+                }
+                else if(player == 1)
+                {
+                    player = 2;
+                    EnterXorO(player,input);
+                }
+                Console.Clear();
+                SetField();
+
+
+                do
+                {
+                    Console.Write("Player {0} Enter the position: ", player);
+                    try
+                    {
+                        input = Convert.ToInt32(Console.ReadLine());
+
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter a a valid number: ");
+                    }
+
+                    if ((input == 1) && (playField[0, 0] == '1'))
+                        inputCorrect = true;
+                    else if ((input == 2) && (playField[0, 1] == '2'))
+                        inputCorrect = true;
+                    else if ((input ==3) && (playField[0, 2] == '3'))
+                        inputCorrect = true;
+                    else if ((input == 4) && (playField[1, 0] == '4'))
+                        inputCorrect = true;
+                    else if ((input == 5) && (playField[1, 1] == '5'))
+                        inputCorrect = true;
+                    else if ((input == 6) && (playField[1, 2] == '6'))
+                        inputCorrect = true;
+                    else if ((input == 7) && (playField[2, 0] == '7'))
+                        inputCorrect = true;
+                    else if ((input == 8) && (playField[2, 1] == '8'))
+                        inputCorrect = true;
+                    else if ((input == 9) && (playField[2, 2] == '9'))
+                        inputCorrect = true;
+                    else
+                    {
+                        Console.WriteLine("\n Incorrect Input choose another field!");
+                        inputCorrect = false;
+                    }
+
+
+                } while (!inputCorrect);
+            } while (true);
         }
+
         public static void SetField()
         {
             Console.WriteLine("     |     |     ");
-            //Todo replace with vars
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", playField[0,0],playField[0,1],playField[0,2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-
-            //Todo replace with vars
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", playField[1,0], playField[1,1], playField[1,2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-
-            //Todo replace with vars
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", 1, 2, 3);
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", playField[2,0], playField[2,1], playField[2,2]);
             Console.WriteLine("     |     |     ");
-
+        }
+        public static void EnterXorO(int player, int input)
+        {
+            char playerSign = ' ';
+            if (player == 1)
+            {
+                playerSign = 'X';
+            }
+            else if(player ==2)
+            {
+                playerSign = 'O';
+            }
+            switch(input){
+                case 1: playField[0, 0] = playerSign; break;
+                case 2: playField[0, 1] = playerSign; break;
+                case 3: playField[0, 2] = playerSign; break;
+                case 4: playField[1, 0] = playerSign; break;
+                case 5: playField[1, 1] = playerSign; break;
+                case 6: playField[1, 2] = playerSign; break;
+                case 7: playField[2, 0] = playerSign; break;
+                case 8: playField[2, 1] = playerSign; break;
+                case 9: playField[2, 2] = playerSign; break;
+            }
         }
     }
 }
